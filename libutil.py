9a7ef3aaea954c89e2104f74a006b7c53aca51d0
Leo van Kampenhout <l.vankampenhout@uu.nl>
2019-03-30 03:30:07 -0600

    update

--- libutil.py
+++ libutil.py
@@ -40,3 +40,30 @@ def print_CPL_icemask_areas(ds,
     print('CPL-G, :', (xgIceMask.to_masked_array() * glc_area_km.values).sum())
     print('CPL-L, :', (xlIceMask.to_masked_array() * lnd_area_km.values).sum())
 
+    
+def print_CPL_glaciercover_areas(ds,
+                            area_xg='domg_aream', area_xl='doml_aream',
+                            fldname_xg='g2x_Sg_ice_covered', fldname_xl='x2l_Sg_ice_covered'):
+    """
+    TODO: NOT IMPLEMENTED
+    """
+    glc_area = ds[area_xg].squeeze()
+    lnd_area = ds[area_xl].squeeze()
+
+    # convert area to km^2
+    glc_area_km = area_rad_to_km(glc_area) 
+    lnd_area_km = area_rad_to_km(lnd_area)
+    
+    # xg 
+    xgIceCov = ds[fldname_xg].squeeze()
+    
+    # xl
+    xlIceCov = np.ma.zeros(lnd_area.shape)
+    varname_pattern = fldname_xl + '{ec:02d}' # EC variable
+
+    for i in range(1,11): # loop over ECs
+        varname = varname_pattern.format(ec=i)
+        xlIceCov += ds[varname].squeeze().to_masked_array()
+        
+    print('CPL-G, :', (xgIceCov.to_masked_array() * glc_area_km.values).sum())
+    print('CPL-L, :', (xlIceCov * lnd_area_km.values).sum())
